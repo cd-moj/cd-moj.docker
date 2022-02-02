43a5607c027bfd63474d714c5e5c7343c0a5d822
Luciano Santos <luciano_z7@hotmail.com>
2022-02-01 22:29:17 -0300

    Adding change into install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,11 +4,10 @@ echo "Bem vindo ao instalador do CD-MOJ!"
 sleep 3
 
 echo "Criando pastas"
+cd cdmoj-dev
 chmod a+rwx cdmoj-dev
-mkdir cd-moj
-cd cd-moj
-mkdir moj-serverside 
-mkdir moj-pagina
+mkdir cdmoj
+cd cdmoj
 mkdir moj
 mkdir moj/submissions
 mkdir moj/contests
@@ -16,7 +15,17 @@ mkdir tmp
 chmod o+w moj/submissions
 chmod o+w tmp
 cd ..
-cp -r examples/* cd-moj/moj/contests/
+cp -r examples/* cdmoj/moj/contests/
+
+echo "Clonando repositorio do cdmoj"
+cd cdmoj-dev
+git clone https://github.com/cd-moj/cdmoj.git cd-moj
+cd cd-moj
+[chmod +x install.sh]
+bash install.sh /cdmoj-dev/cdmoj/moj-serverside  /cdmoj-dev/cdmoj/moj-pagina 
+cd ..
+cp config/common.conf /cdmoj-dev/cdmoj/moj-serverside/etc/
+rm -rf cd-moj
 
 echo "Substituindo arquivos"
 cp config/apache2.conf /etc/apache2/
@@ -29,22 +38,17 @@ a2dissite 000-default
 a2ensite moj
 a2enmod include
 
-echo "Clonando repositorio do cdmoj"
-cd cdmoj-dev
-git clone https://github.com/cd-moj/cdmoj.git
-cd cdmoj
-#cd /home/$USER/CD-MOJ
-chmod +x install.sh
-bash install.sh /cdmoj-dev/cd-moj/moj-serverside  /cdmoj-dev/cd-moj/moj-pagina 
-cd ..
 
-cp config/common.conf /cdmoj-dev/cd-moj/moj-serverside/etc/
+
 echo "ServerName localhost" >> /etc/apache2/apache2.conf
 
 echo "Iniciando apache2"
 service apache2 start
 
 echo "Iniciando daemons"
-#ls 
-#bash cd-moj/moj-serverside/daemons/executar-corretor.sh
-#bash cd-moj/moj-serverside/daemons/executar-julgador.sh
+ls 
+#touch /dev/executar-corretor
+#touch /dev/executar-julgador
+#bash cd-moj/moj-serverside/daemons/executar-corretor.sh </dev/executar-corretor &>/dev/executar-corretor &
+#bash cd-moj/moj-serverside/daemons/executar-julgador.sh </dev/executar-julgador &>/dev/executar-julgador &
+#service apache2 start
